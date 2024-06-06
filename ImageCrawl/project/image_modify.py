3ea4c16cd5d3362c72f9314d252c215b9604f1da
Hinsam <inshsam3@gmail.com>
2024-06-07 00:11:34 +0900

    Update image_modify.py
    .

--- ImageCrawl/project/image_modify.py
+++ ImageCrawl/project/image_modify.py
@@ -2,12 +2,14 @@ import pytesseract
 import re
 import scrapy
 from io import BytesIO
-from PIL import Image, UnidentifiedImageError
+from PIL import Image, ImageSequence
 from scrapy.http import Request
 from urllib.parse import urljoin
 from collections import deque, Counter
+from ..items import GetWordsItem
 
 #pytesseract 사용 전 https://github.com/UB-Mannheim/tesseract/wiki 설치!!!!
+#테서랙트 경로
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 
@@ -16,79 +18,82 @@ class ImageSpider(scrapy.Spider):
 
     def __init__(self, *args, **kwargs):
         super(ImageSpider, self).__init__(*args, **kwargs)
-        self.divide_file = open('divide.txt', 'w') 
-        self.image_file = open('image.txt', 'w')
-        self.hosts_queue = deque(open('D:/hosts.txt', 'r', encoding='utf-8').read().strip().split("\n"))
-        self.logger.info(f"Loaded {len(self.hosts_queue)} URLs from hosts.txt")
+        self.divide_file = open('divide.txt', 'w', encoding='utf-8') #단어 추출이 잘 되었는지 확인하기 위함
+        self.image_file = open('image.txt', 'w', encoding='utf-8') #이미지 추출이 잘 되었는지 확인하기 위함
+        self.hosts_queue = deque(open('hosts.txt', 'r', encoding='utf-8').read().strip().split("\n")) #hosts.txt에서 값을 불러와서 큐에 넣기
 
 
     def start_requests(self):
-        if self.hosts_queue:
-            current_url = self.hosts_queue.popleft()
-            yield scrapy.Request(url=current_url, callback=self.parse, errback=self.errback)
+        if self.hosts_queue:  #큐가 비어있지 않다면
+            current_url = self.hosts_queue.popleft() #맨 앞에서 시작
+            yield scrapy.Request(url=current_url, callback=self.parse, errback=self.errback, dont_filter=True, meta={'original_url': current_url})
 
 
     def parse(self, response):
-        current_url = response.url
+        original_url = response.meta.get('original_url') #큐에서 가져온 오리지널 경로
         texts = []
 
-# 1) 이부분 참고 부탁드려요!!
-        image_texts = self.extract_in_image(response, current_url)
-        yield from (text for text in image_texts)
-        if image_texts:
-            texts.extend(image_texts)
-# 여기까지
-
-        count_words = self.extract_words_count(texts)
-        self.save_words_count(current_url, count_words) 
-
+        image_texts = list(self.extract_in_image(response, original_url))  # 리스트형태로 이미지에서 글씨 뽑아오기
+        for future in image_texts:
+            res = yield future
+            if res and 'gettext' in res.meta:
+                texts.extend(res.meta['gettext'])
         if self.hosts_queue:
             next_url = self.hosts_queue.popleft()
-            yield scrapy.Request(url=next_url, callback=self.parse, errback=self.errback)
+            yield scrapy.Request(url=next_url, callback=self.parse, errback=self.errback, dont_filter=True, meta={'original_url': next_url})
 
-    def extract_in_image(self, response, current_url):
-        self.image_file.write(f"{current_url}\n")  
-        img_urls = response.css('img::attr(src)').extract()
+
+    # 이미지에서 단어 추출하는 코드
+    def extract_in_image(self, response, original_url):
+        self.image_file.write(f"{original_url}\n")
+        #수정: src 속성은 이미지를 즉시 로드하여 표시하는 데 사용되고, data-original 속성은 이미지를 나중에 필요할 때 로드하는 데 사용
+        img_urls = response.css('img::attr(src), img::attr(data-original)').extract()
+        print(img_urls, len(img_urls))
         for img_url in img_urls:
             if not img_url.startswith(('http', 'https')):
                 img_url = urljoin(response.url, img_url)
-            self.logger.info(f"Queuing image for download: {img_url}")
-            yield Request(img_url, callback=self.parse_image, meta={'img_url': img_url, 'current_url': current_url})
+            yield Request(img_url, callback=self.parse_image, meta={'img_url': img_url, 'original_url': original_url})
 
     def parse_image(self, response):
         img_url = response.meta['img_url']
-        current_url = response.meta['current_url']
-        self.logger.info(f"Processing image: {img_url}")
+        original_url = response.meta['original_url']
 
         try:
             img = Image.open(BytesIO(response.body))
-
             if img.format not in ['JPEG', 'PNG', 'GIF']:
-                self.logger.error(f"Unsupported image format: {img.format}")
                 return
 
-            min_image_width = 50  
-            min_image_height = 50  
+            min_image_width = 50
+            min_image_height = 50
             width, height = img.size
-            self.logger.info(f"Image size: {width}x{height}")
 
-# 2) kor+eng로 수정
             if width > min_image_width and height > min_image_height:
-                text = pytesseract.image_to_string(img, lang='kor+eng')
-                gettext = self.process_text(text)
-                self.logger.info(f"Extracted text: {gettext}")
+                #수정: gif파일일 경우 마지막 프레임만 추출하기(대략적인 내용은 마지막에만 담겨있음)
+                if img.format == 'GIF':
+                    frames = ImageSequence.Iterator(img)
+                    for frame in frames:
+                        last_frame = frame
+                    text = pytesseract.image_to_string(last_frame, lang='kor+eng')
+                else:
+                    text = pytesseract.image_to_string(img, lang='kor+eng')
 
+                gettext = self.process_text(text)
                 if gettext:
                     self.image_file.write(f"{img_url} : {', '.join(gettext)}\n")
-                    return gettext
-        except UnidentifiedImageError as e:
-            self.logger.error(f"An error occurred while processing the image: cannot identify image file {e}")
-        except IOError as e:
-            self.logger.error(f"An error occurred while processing the image: {e}")
+                    count_words = self.extract_words_count(gettext)
+
+                    for word, count in count_words.items():
+                        item = GetWordsItem()
+                        item['host'] = original_url
+                        item['redirect'] = None
+                        item['words'] = word
+                        item['count'] = count
+                        yield item
+        except Exception as e:
+            return
 
     def process_text(self, text):
         text = re.sub(r'\b[ㄱ-ㅎㅏ-ㅣ]\b', '', text)
-        # 3) 여기 re.sub('[a-zA-Z0-9]', '', text) 이렇게 되어 있을텐데 [0-9] 이걸로 수정 부탁드립니다.
         text = re.sub('[0-9]', '', text)
         text = re.sub(r'[^\w\s]', '', text)
         text = re.sub(r'\d+', '', text)
@@ -101,17 +106,11 @@ class ImageSpider(scrapy.Spider):
     def extract_words_count(self, words):
         return dict(Counter(words))
 
-    def save_words_count(self, url, count_words):
-        self.logger.info(f"Saving word counts for URL: {url}")
-        for word, count in count_words.items():
-            self.divide_file.write(f"{url} ---> {word}: {count}\n")
-
     def errback(self, failure):
-        self.logger.error(repr(failure))
         if self.hosts_queue:
             next_url = self.hosts_queue.popleft()
-            self.logger.info(f"Queueing next URL after error: {next_url}")
-            yield scrapy.Request(url=next_url, callback=self.parse, errback=self.errback)
+            yield scrapy.Request(url=next_url, callback=self.parse, errback=self.errback, dont_filter=True, meta={'original_url': next_url})
 
     def closed(self, reason):
         self.divide_file.close()
+        self.image_file.close()
